4ea8eee3e8809f7f3be9b0648a1e17238500cd31
Abbas Ansari <iamabbas.self@gmail.com>
2026-02-13 19:26:54 +0530

    add Notification Service Implementation

---
+++ notification-service/src/main/java/com/nexpay/notificationservice/service/NotificationServiceImpl.java
@@ -0,0 +1,27 @@
+package com.nexpay.notificationservice.service;
+
+import com.nexpay.notificationservice.entity.Notification;
+import com.nexpay.notificationservice.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class NotificationServiceImpl implements NotificationService{
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Override
+    public Notification sendNotification(Notification notification) {
+        notification.setSentAt(LocalDateTime.now());
+        return notificationRepository.save(notification);
+    }
+
+    @Override
+    public List<Notification> getNotificationsByUserId(Long userId) {
+        return notificationRepository.findByUserId(userId);
+    }
+}
