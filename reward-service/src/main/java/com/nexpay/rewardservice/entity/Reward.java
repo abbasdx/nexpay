fd78ee064d379a9d26c8d9ef542d5175bd5b57b0
Abbas Ansari <iamabbas.self@gmail.com>
2026-04-10 23:08:34 +0530

    add reward service

---
+++ reward-service/src/main/java/com/nexpay/rewardservice/entity/Reward.java
@@ -0,0 +1,30 @@
+package com.nexpay.rewardservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reward")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Reward {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long UserId;
+
+    private Double points;
+
+    private LocalDateTime sentAt;
+
+    @Column(unique = true)
+    private Long transactionId;
+    
+}
