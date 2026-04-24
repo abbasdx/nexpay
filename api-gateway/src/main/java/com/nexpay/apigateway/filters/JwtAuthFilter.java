0355346f892238f4cac8ca9ee9a462814d30ec17
Abbas Ansari <iamabbas.self@gmail.com>
2026-04-24 23:17:21 +0530

    add JwtAuthFilter

---
+++ api-gateway/src/main/java/com/nexpay/apigateway/filters/JwtAuthFilter.java
@@ -0,0 +1,64 @@
+package com.nexpay.apigateway.filters;
+
+import com.nexpay.apigateway.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter  implements GlobalFilter, Ordered {
+
+    private static final List<String> PUBLIC_PATHS = List.of(
+            "/auth/signup",
+            "/auth/login"
+    );
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().value();
+        String normalizedPath = path.replaceAll("/+$", "");
+        if (PUBLIC_PATHS.contains(normalizedPath)) {
+            return chain.filter(exchange)
+                    .doOnSubscribe(s -> System.out.println("Proceeding without check"))
+                    .doOnSuccess(s -> System.out.println("Successfully proceeded without check"))
+                    .doOnError(e -> System.err.println("Error Occurred"));
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();        }
+
+        try{
+            String token = authHeader.substring(7);
+            Claims claims = JwtUtil.validateToken(token);
+            exchange.getRequest().mutate()
+                    .header("X-User-Email", claims.getSubject())
+                    .header("X-User-Id", claims.get("userId", String.class))
+                    .header("X-User-Role", claims.get("role", String.class))
+                    .build();
+
+            return chain.filter(exchange)
+                    .doOnSubscribe(s -> System.out.println("Proceeding without check"))
+                    .doOnSuccess(s -> System.out.println("Successfully proceeded without check"))
+                    .doOnError(e -> System.err.println("Error Occurred"));
+        }catch(Exception e){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return  exchange.getResponse().setComplete();
+        }
+
+    }
+
+    @Override
+    public int getOrder() {
+        return -100;
+    }
+}
