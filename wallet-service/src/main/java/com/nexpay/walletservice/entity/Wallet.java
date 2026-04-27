c5c45995fca9b627a4e739f073a07bc849ace8d8
Abbas Ansari <iamabbas.self@gmail.com>
2026-04-27 20:27:18 +0530

    add wallet entity

---
+++ wallet-service/src/main/java/com/nexpay/walletservice/entity/Wallet.java
@@ -0,0 +1,47 @@
+package com.nexpay.walletservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wallets")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long userId;
+
+    @Column(nullable = false, length = 3)
+    private String currency = "INR";
+
+    @Column(nullable = false)
+    private Long balance = 0L;
+
+    @Column(nullable = false)
+    private Long availableBalance = 0L;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Wallet(Long userId, String currency) {
+        this.userId = userId;
+        this.currency = currency;
+        this.balance = 0L;
+        this.availableBalance = 0L;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
