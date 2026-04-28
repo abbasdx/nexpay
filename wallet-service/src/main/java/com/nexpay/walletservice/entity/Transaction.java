20ace7130a781f3d9ee30a37519942a9b4987cc5
Abbas Ansari <iamabbas.self@gmail.com>
2026-04-28 19:18:42 +0530

    add transation entity

---
+++ wallet-service/src/main/java/com/nexpay/walletservice/entity/Transaction.java
@@ -0,0 +1,45 @@
+package com.nexpay.walletservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long walletId;
+
+    @Column(nullable = false)
+    private String type; // CREDIT, DEBIT, HOLD, RELEASE, CAPTURE
+
+    @Column(nullable = false)
+    private Long amount; // stored in paise/cents
+
+    @Column(nullable = false)
+    private String status; // SUCCESS, FAILED, EXPIRED
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Transaction() {}
+
+    public Transaction(Long walletId, String type, Long amount, String status) {
+        this.walletId = walletId;
+        this.type = type;
+        this.amount = amount;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
+
