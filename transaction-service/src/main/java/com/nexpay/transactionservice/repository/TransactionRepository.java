f540d725785ee2636b6fda7c4e1a62cbacdf544a
Abbas Ansari <iamabbas.self@gmail.com>
2026-05-14 21:18:37 +0530

    update transaction repository

--- transaction-service/src/main/java/com/nexpay/transactionservice/repository/TransactionRepository.java
+++ transaction-service/src/main/java/com/nexpay/transactionservice/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.nexpay.transactionservice.repository;
 import com.nexpay.transactionservice.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findBySenderIdOrReceiverId(Long senderId, Long receiverId);
+
 }
