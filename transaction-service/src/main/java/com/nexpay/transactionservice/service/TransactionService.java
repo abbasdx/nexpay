fb2d5252e47b38cd1a4ee31ad0c426138a5fda2f
Abbas Ansari <iamabbas.self@gmail.com>
2026-05-22 20:26:41 +0530

    update transaction service

--- transaction-service/src/main/java/com/nexpay/transactionservice/service/TransactionService.java
+++ transaction-service/src/main/java/com/nexpay/transactionservice/service/TransactionService.java
@@ -1,10 +1,11 @@
 package com.nexpay.transactionservice.service;
 
+import com.nexpay.transactionservice.dto.TransactionResponse;
 import com.nexpay.transactionservice.entity.Transaction;
 import java.util.List;
 
 public interface TransactionService {
-    Transaction createTransaction(Transaction transaction);
-
-    List<Transaction> getAllTransactions();
+    TransactionResponse createTransaction(Transaction transaction);
+    public Transaction getTransactionById(Long id);
+    public List<Transaction> getTransactionsByUser(Long userId);
 }
