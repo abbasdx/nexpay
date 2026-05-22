aff3212a4fed34bdb12ca7494a0da1692b85ce77
Abbas Ansari <iamabbas.self@gmail.com>
2026-05-22 20:34:41 +0530

    update TransactionServiceApplication

--- transaction-service/src/main/java/com/nexpay/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/nexpay/transactionservice/TransactionServiceApplication.java
@@ -2,8 +2,10 @@ package com.nexpay.transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.nexpay.transactionservice.client")
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
