a0fbee66257e43337f5335a9e5e89a4f6a9bf919
Abbas Ansari <iamabbas.self@gmail.com>
2026-05-28 21:12:34 +0530

    update reward repository

--- reward-service/src/main/java/com/nexpay/rewardservice/repository/RewardRepository.java
+++ reward-service/src/main/java/com/nexpay/rewardservice/repository/RewardRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RewardRepository extends JpaRepository<Reward, Long> {
     List<Reward> findByUserId(Long userId);
-
     Boolean existsByTransactionId(Long transactionId);
+    Optional<Reward> findByTransactionId(Long transactionId); 
+
 }
