f073a4a3dd375d6e825912b75a48e2b72a5255a0
Abbas Ansari <iamabbas.self@gmail.com>
2026-05-31 21:13:43 +0530

    update KafkaConsumer

--- reward-service/src/main/java/com/nexpay/rewardservice/config/KafkaConsumer.java
+++ reward-service/src/main/java/com/nexpay/rewardservice/config/KafkaConsumer.java
@@ -24,7 +24,7 @@ public class KafkaConsumer {
         deserializer.addTrustedPackages("com.nexpay.transactionservice.entity");
         Map<String, Object> props = new HashMap<>();
 
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "reward-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
