6149257b0648a14b4c3583c0b3f14e8e91f6e8d5
Abbas Ansari <iamabbas.self@gmail.com>
2026-06-01 20:47:15 +0530

    update reward controller

--- reward-service/src/main/java/com/nexpay/rewardservice/controller/RewardController.java
+++ reward-service/src/main/java/com/nexpay/rewardservice/controller/RewardController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/rewards")
-@CrossOrigin(origins = "http://localhost:3000")
 public class RewardController {
 
     private final RewardRepository rewardRepository;
